eeb82a819c8deb5379afac612251349eb96a75cb
Daraganov Git-71 <denis.daraganov2013@yandex.ru>
2023-07-08 19:42:40 +0300

    modification

---
+++ src/main/java/ru/netology/Epic.java
@@ -0,0 +1,24 @@
+package ru.netology;
+
+public class Epic extends Task {
+    protected String[] subtasks;
+
+    public Epic(int id, String[] subtasks) {
+        super(id);
+        this.subtasks = subtasks;
+    }
+
+    public String[] getSubtasks() {
+        return subtasks;
+    }
+
+    @Override
+    public boolean matches(String query) {        //метод поискового запроса
+        for (String verifiableTask : subtasks) {
+            if (verifiableTask.contains(query)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
